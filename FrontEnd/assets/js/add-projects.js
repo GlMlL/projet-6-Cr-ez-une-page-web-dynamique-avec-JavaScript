8bc38bb6918e62b8aca5a0ab8262fd90a8e5e669
arnaud guillemin <arnaud.guillemin27@gmail.com>
2024-05-03 10:57:09 +0200

    modif

--- FrontEnd/assets/js/add-projects.js
+++ FrontEnd/assets/js/add-projects.js
@@ -1,20 +1,31 @@
 // Récupérer le formulaire et le bouton d'ajout de photo
-const photoForm = document.getElementById("photo-form");
+
 const addPhotoButton = document.querySelector(".upload-btn");
 
-// Désactiver le bouton d'ajout de photo par défaut
-addPhotoButton.disabled = true;
 
-// Fonction pour gérer la sélection de fichier
-function handleFileSelection() {
-    const files = document.getElementById("image").files;
-    
-    // Activer le bouton si des fichiers sont sélectionnés, sinon le désactiver
-    addPhotoButton.disabled = files.length === 0;
-}
 
-// Ajouter un écouteur d'événements sur le changement de sélection de fichier
-document.getElementById("image").addEventListener("change", handleFileSelection);
+document.getElementById("image").addEventListener("change", () => {
+    const file = document.getElementById("image").files[0];
+    const previewImage = document.getElementById("preview-image");
+    const previewBox = document.querySelector(".preview"); //div qui englobe l'image et le texte
+
+    // Vérifier si un fichier est sélectionné
+    if (file) {
+        // Créer un objet URL pour l'image sélectionnée
+        const imageURL = URL.createObjectURL(file);
+
+        // Afficher l'image en prévisualisation
+        previewImage.src = imageURL;
+        previewBox.classList.toggle('hidden');
+        document.querySelector(".upload-content").classList.toggle('hidden');
+
+       
+    } else {
+        // Cacher l'image et le texte de prévisualisation si aucun fichier n'est sélectionné
+        
+        previewBox.classList.add('hidden');
+    }
+});
 
 // Fonction pour gérer la soumission du formulaire
 function onPhotoSubmit(event) {
@@ -22,31 +33,31 @@ function onPhotoSubmit(event) {
 
     // Récupérer les données du formulaire
     const formData = new FormData(event.target);
+   
 
     // Envoyer les données du formulaire à l'API
     const adminToken = sessionStorage.getItem("Token");
     fetch("http://localhost:5678/api/works", {
         method: "POST",
         headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${adminToken}`, // Utilisation du token récupéré depuis la session
+          Accept: "application/json",
+          Authorization: `Bearer ${adminToken}`, // Utilisation du token récupéré depuis la session
+          
         },
         body: formData,
+      })
+    .then(response => {
+        if (response.ok) {
+            alert("Photo ajoutée avec succès !");
+            // Rafraîchir la galerie après l'ajout de la photo (si nécessaire)
+            displayGalleryContent();
+        } else {
+            alert("Une erreur s'est produite lors de l'ajout de la photo.");
+        }
     })
-        .then(response => {
-            if (response.ok) {
-                alert("Photo ajoutée avec succès !");
-                // Rafraîchir la galerie après l'ajout de la photo (si nécessaire)
-                displayGalleryContent();
-            } else {
-                alert("Une erreur s'est produite lors de l'ajout de la photo.");
-            }
-        })
-        .catch(error => {
-            console.error("Une erreur s'est produite lors de l'ajout de la photo :", error);
-        });
+    .catch(error => {
+        console.error("Une erreur s'est produite lors de l'ajout de la photo :", error);
+    });
 }
-
-// Ajouter un écouteur d'événements pour la soumission du formulaire
-photoForm.addEventListener('submit', onPhotoSubmit);
+document.getElementById("photo-form").addEventListener( 'submit', onPhotoSubmit);
 
